f7c4f411781ec20ba6eeb438f895759d89768ce4
Kelvin Zee <101116613+zeekelvin@users.noreply.github.com>
2025-07-02 00:10:45 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FFD700', // Yellow
+        background: '#000000', // Black
+      },
+    },
+  },
+  plugins: [],
+};
